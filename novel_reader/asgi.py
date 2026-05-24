b82f7ed8704f3f1d1c25640b092db769aa487a8d
lobster-xiaozhua <3567868452@qq.com>
2026-05-24 16:26:30 +0800

    feat: 检查 ASGI 服务器 Bug (#16)
    Co-authored-by: lobster-xiaozhua <lobster-xiaozhua@users.noreply.github.com>
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- novel_reader/asgi.py
+++ novel_reader/asgi.py
@@ -1,5 +1,20 @@
 import os
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novel_reader.settings')
-application = get_asgi_application()
+
+_django_app = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    if scope['type'] == 'lifespan':
+        while True:
+            message = await receive()
+            if message['type'] == 'lifespan.startup':
+                await send({'type': 'lifespan.startup.complete'})
+            elif message['type'] == 'lifespan.shutdown':
+                await send({'type': 'lifespan.shutdown.complete'})
+                return
+    else:
+        await _django_app(scope, receive, send)
